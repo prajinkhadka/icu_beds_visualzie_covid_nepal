90bb1bd73ce5f2279d0f5f441529660f8c51f877
Prajin Khadka <34759925+prajinkhadka@users.noreply.github.com>
2021-04-28 21:36:35 +0545

    Update

--- run.py
+++ run.py
@@ -6,27 +6,49 @@ warnings.filterwarnings("ignore")
 from datetime import datetime, date, time
 import os 
 
-from main import data_from_date
-data = pd.read_csv("Nepal_ICU_Ventilators - Sheet1 (7).csv")
+from main import data_from_date, get_data_hospital_gandaki
+data = pd.read_csv("main.csv")
 start_date = date(2021, 4, 12)
 
 def main():
     st.sidebar.title("Choose the State")
     app_mode = st.sidebar.selectbox("",
-        ["Choose State", "Pradesh-1", "Pradesh-2", "Bagmati", "Gandaki", "Lumbini", "Karnali", "Sudur-Paschim"])
+        ["Choose State", "Pradesh-1", "Pradesh-2", "Bagmati", "Gandaki", "Lumbini", "Karnali", "Sudur-Paschim"], key="123")
     
     st.text("Choose date after April 12 2021")
     date = st.date_input('Date')
     print("type", type(date))
+    print(date)
     print("type", type(start_date))
+    
+    if app_mode == "Gandaki":
+        hosp_name = st.selectbox("",
+        ["Choose Hospital", 'Pachhimanchal Community Hospital', 'Mediplus Hospital',
+       'Parkland Hospital', 'Gandaki Medical College',
+       'Fewacity Hospital', 'Galaxy Hospital', 'Metrocity Hospital',
+       'Kaski sewa Hospital', 'Fishtail Hospital', 'Charak Memorial',
+       'Lakecity Hospital', 'Pokhara Academy of Health Science',
+       'Manipal Teaching Hospital ',
+       'Infectious and Communicable Disease Hospital', 'Namaste Hospital',
+       'Dhaulagiri Hospital', 'Madhyabindu Hospital',
+       'Parbat District Hospital', 'Gorkha District Hospital',
+       'Damauli Hospital', 'Lamjung Community Hospital', 'Beni Hosputal'])
+
+
+
     process = st.button("Process")
+
     if process:
-        data_from_date(data = data, curring_date=date, start_date=start_date, state= "app_mode")
+        if app_mode == "Gandaki":
+            get_data_hospital_gandaki(hospital_name=hosp_name, date = str(date))
+            st.image("Image2.png")
+            os.remove("Image2.png")
+        else:
+            data_from_date(data = data, curring_date=date, start_date=start_date, state= "app_mode")
+            st.image("Image.png")
+            os.remove("Image.png")
 
-        st.image("Image.png")
-        os.remove("Image.png")
-
-    st.text("Github Link:")
+    st.text("Github Link: https://github.com/prajinkhadka/icu_beds_visualzie_covid_nepal")
 
 
 if __name__ == "__main__":
